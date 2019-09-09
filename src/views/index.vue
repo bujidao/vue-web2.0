958fc128dabc72181c06f34f1bbf55b604786f81
AlexCeng <izengjing.cn@gmail.com>
2019-09-09 16:43:17 +0800

    repair[sass]

---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'i am vue demo'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
